ea8b981da15991e650481e4a7304b417d7e721ef
Liviu Dobre <liviu.dobre.ext@siemens-energy.com>
2024-03-03 08:04:40 +0200

    add some exceptions handling

--- app/main.py
+++ app/main.py
@@ -1,29 +1,39 @@
 import os
 from typing import Annotated
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path,
+from starlette.exceptions import HTTPException
 from vdriver import VDriver
 
 app = FastAPI()
 @app.get("/clear_db")
 def clear_content():
-    v = VDriver().clear_db()
-
+    result = VDriver().clear_db()
+    if result is False:
+        raise HTTPException(status_code=400, detail="Usually bad credentials for neo4j database or embeddings settings")
+    return {"Content cleared"}
 
 @app.get("/load_content_to_db")
 def load_content():
     v = VDriver()
     content = ""
-    for file in os.listdir('wiki'):
-        if file.endswith(".txt"):
-            with open(f'wiki/{file}') as f:
-                content += f.read()
-
+    try:
+        for file in os.listdir('wiki'):
+            if file.endswith(".txt"):
+                with open(f'wiki/{file}') as f:
+                    content += f.read()
+    except FileNotFoundError:
+        raise HTTPException(status_code=400, detail="folder wiki not found to process")
     results = v.vectorize(content=content)
+    if results is False:
+        raise HTTPException(status_code=400, detail="Usually bad credentials for neo4j database")
     return {"Content Loaded": results }
-
 
 @app.get("/search/{query}/{k}")
 def search(query: str, k:Annotated[int, Path(title="top K results", ge=1)]):
     v = VDriver()
     docs_with_score = v.query(query=query, size=k)
-    return [(document.page_content, score) for document, score in docs_with_score]
+    if docs_with_score is not False:
+        return [(document.page_content, score) for document, score in docs_with_score]
+
+    raise HTTPException(status_code=400, detail="Usually bad credentials for neo4j database or embeddings settings")
+
--- app/vdriver.py
+++ app/vdriver.py
@@ -4,6 +4,7 @@ from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from config import (EMBEDDING_MODEL , NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD,
                     NEO4J_DB, NEO4J_INDEX,NEO4J_NODE_LABEL, NEO4J_TXTNODE_PROP,
                     NEO4J_EMBNODE_PROP)
+from neo4j.exceptions import ConfigurationError
 
 class VDriver(object):
     """ This is a class that allows you to vectorize a text using Neo4j, langcain,
@@ -12,51 +13,77 @@ class VDriver(object):
     def __init__(self):
         model_kwargs = {"device": "cpu"}
         encode_kwargs = {"normalize_embeddings": True}
-        self.embeddings = HuggingFaceBgeEmbeddings(
-            model_name=EMBEDDING_MODEL, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
-        )
+        try:
+            self.__embeddings = HuggingFaceBgeEmbeddings(
+                model_name=EMBEDDING_MODEL, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+            )
+        except EnvironmentError:
+            self.__embeddings = False
     def __text_splitter(self):
         """ This method use SemanticChunker to split the text into sentences"""
-        return SemanticChunker(self.embeddings)
+        if self.__embeddings:
+            return SemanticChunker(self.__embeddings)
+        return False
 
     def __create_documents(self, content):
         """ This method creates a documents' object from a text """
-        return self.__text_splitter().create_documents([content])
+        text_splitter = self.__text_splitter()
+        if text_splitter is not False:
+            return text_splitter.create_documents([content])
+        return False
 
     def __get_db_from_existing_index(self):
         """ This method loads a Neo4j index from the existing one in the db"""
-        return Neo4jVector.from_existing_index(
-            self.embeddings,
-            url=NEO4J_URI,
-            username=NEO4J_USERNAME,
-            password=NEO4J_PASSWORD,
-            database = NEO4J_DB,
-            index_name = NEO4J_INDEX,
-            node_label=NEO4J_NODE_LABEL,
-            text_node_property=NEO4J_TXTNODE_PROP,
-            embedding_node_property=NEO4J_EMBNODE_PROP,
-        )
-
+        if self.__embeddings:
+            try:
+                return Neo4jVector.from_existing_index(
+                    self.__embeddings,
+                    url=NEO4J_URI,
+                    username=NEO4J_USERNAME,
+                    password=NEO4J_PASSWORD,
+                    database = NEO4J_DB,
+                    index_name = NEO4J_INDEX,
+                    node_label=NEO4J_NODE_LABEL,
+                    text_node_property=NEO4J_TXTNODE_PROP,
+                    embedding_node_property=NEO4J_EMBNODE_PROP,
+                )
+            except  (ConfigurationError,ValueError):
+                return False
+        return False
     def clear_db(self):
         """ This method clears the Neo4j database after loading the vectors"""
-        return self.__get_db_from_existing_index().query("MATCH(n) DETACH DELETE n")
+        db = self.__get_db_from_existing_index()
+        if db is not False:
+            return db.query("MATCH(n) DETACH DELETE n")
+        return False
 
     def vectorize(self, content):
         """ This method clear first the existing index and then vectorize the documents provided by the content"""
         self.clear_db()
         docs = self.__create_documents(content)
-        db = Neo4jVector.from_documents(
-            docs, self.embeddings, url=NEO4J_URI,
-            username=NEO4J_USERNAME,
-            password=NEO4J_PASSWORD,
-            database=NEO4J_DB,
-            index_name=NEO4J_INDEX,
-            node_label=NEO4J_NODE_LABEL,
-            text_node_property=NEO4J_TXTNODE_PROP,
-            embedding_node_property=NEO4J_EMBNODE_PROP,
-            create_id_index=True,
-        )
-        return True
+        if docs:
+            try:
+                db = Neo4jVector.from_documents(
+                    docs, self.__embeddings, url=NEO4J_URI,
+                    username=NEO4J_USERNAME,
+                    password=NEO4J_PASSWORD,
+                    database=NEO4J_DB,
+                    index_name=NEO4J_INDEX,
+                    node_label=NEO4J_NODE_LABEL,
+                    text_node_property=NEO4J_TXTNODE_PROP,
+                    embedding_node_property=NEO4J_EMBNODE_PROP,
+                    create_id_index=True,
+                )
+                return True
+            except  (ConfigurationError, ValueError):
+                return False
+        return False
     def query(self, query, size=5):
         """ This method queries the Neo4j for documents"""
-        return self.__get_db_from_existing_index().similarity_search_with_score(query, k=size)
+        db = self.__get_db_from_existing_index()
+        if db is not False:
+            try:
+                return db.similarity_search_with_score(query, k=size)
+            except (ConfigurationError,ValueError):
+                return False
+        return False
